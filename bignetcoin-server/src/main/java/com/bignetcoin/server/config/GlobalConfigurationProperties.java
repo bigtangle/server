a449e162476ef7fcde292b27f9b9a1066f0a58b8
姜涛\Administrator <jiangtao19890902@sina.com>
2018-03-06 16:10:02 +0800

    修改tipservice 数据库连接

---
+++ bignetcoin-server/src/main/java/com/bignetcoin/server/config/GlobalConfigurationProperties.java
@@ -0,0 +1,49 @@
+package com.bignetcoin.server.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="db")
+public class GlobalConfigurationProperties {
+
+    private String hostname;
+    
+    private String dbName;
+    
+    private String username;
+    
+    private String password;
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- bignetcoin-server/src/test/java/com/bignetcoin/server/TipsServiceTest.java
+++ bignetcoin-server/src/test/java/com/bignetcoin/server/TipsServiceTest.java
@@ -10,15 +10,20 @@ import org.bitcoinj.core.Block;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.FullPrunedBlockGraph;
 import org.bitcoinj.core.MySQLFullPrunedBlockChainTest;
+import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
 import org.bitcoinj.core.TransactionOutPoint;
+import org.bitcoinj.store.BlockStoreException;
+import org.bitcoinj.store.FullPrunedBlockStore;
+import org.bitcoinj.store.MySQLFullPrunedBlockStore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.bignetcoin.server.config.GlobalConfigurationProperties;
 import com.bignetcoin.server.service.BlockService;
 import com.bignetcoin.server.service.TipsService;
 
@@ -37,6 +42,23 @@ public class TipsServiceTest extends MySQLFullPrunedBlockChainTest {
             ECKey outKey = new ECKey();
             System.out.println("prK : " + outKey.getPrivateKeyAsHex() + ", " + outKey.getPublicKeyAsHex());
         }
+    }
+    
+    @Autowired
+    private GlobalConfigurationProperties globalConfigurationProperties;
+    
+    @Override
+    public FullPrunedBlockStore createStore(NetworkParameters params, int blockCount) throws BlockStoreException {
+        try {
+            String DB_HOSTNAME = globalConfigurationProperties.getHostname();
+            String DB_NAME = globalConfigurationProperties.getDbName();
+            String DB_USERNAME = globalConfigurationProperties.getUsername();
+            String DB_PASSWORD = globalConfigurationProperties.getPassword();
+            store = new MySQLFullPrunedBlockStore(params, blockCount, DB_HOSTNAME, DB_NAME, DB_USERNAME, DB_PASSWORD);
+        } catch (RuntimeException e) {
+        }
+        resetStore(store);
+        return store;
     }
 
     @Test
