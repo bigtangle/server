22350b52dcabda0541eec3dcc3534a23f5f17a06
mi <mi@inasset.de>
2018-03-07 12:37:38 +0800

    getbalance test

---
+++ bignetcoin-server/src/test/java/com/bignetcoin/server/TransactionServiceTest.java
@@ -0,0 +1,126 @@
+package com.bignetcoin.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Block;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.FullPrunedBlockGraph;
+import org.bitcoinj.core.MySQLFullPrunedBlockChainTest;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionOutPoint;
+import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.core.UTXO;
+import org.bitcoinj.script.Script;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bignetcoin.server.service.TransactionService;
+import com.google.common.collect.Lists;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TransactionServiceTest extends MySQLFullPrunedBlockChainTest {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Test
+    public void getBalance() throws Exception {
+        final int UNDOABLE_BLOCKS_STORED = 10;
+        store = createStore(PARAMS, UNDOABLE_BLOCKS_STORED);
+        resetStore(store);
+        blockgraph = new FullPrunedBlockGraph(PARAMS, store);
+
+        // Check that we aren't accidentally leaving any references
+        // to the full StoredUndoableBlock's lying around (ie memory leaks)
+        ECKey outKey = new ECKey();
+        int height = 1;
+
+        // Build some blocks on genesis block to create a spendable output
+        Block rollingBlock = PARAMS.getGenesisBlock().createNextBlockWithCoinbase(Block.BLOCK_VERSION_GENESIS,
+                outKey.getPubKey(), height++, PARAMS.getGenesisBlock().getHash());
+        blockgraph.add(rollingBlock);
+        Transaction transaction = rollingBlock.getTransactions().get(0);
+        TransactionOutPoint spendableOutput = new TransactionOutPoint(PARAMS, 0, transaction.getHash());
+        byte[] spendableOutputScriptPubKey = transaction.getOutputs().get(0).getScriptBytes();
+        for (int i = 1; i < PARAMS.getSpendableCoinbaseDepth(); i++) {
+            rollingBlock = rollingBlock.createNextBlockWithCoinbase(Block.BLOCK_VERSION_GENESIS, outKey.getPubKey(),
+                    height++, PARAMS.getGenesisBlock().getHash());
+            blockgraph.add(rollingBlock);
+        }
+        rollingBlock = rollingBlock.createNextBlock(null, PARAMS.getGenesisBlock().getHash());
+
+        // Create bitcoin spend of 1 BTC.
+        ECKey toKey = new ECKey();
+        Coin amount = Coin.valueOf(100000000);
+        Address address = new Address(PARAMS, toKey.getPubKeyHash());
+        Coin totalAmount = Coin.ZERO;
+
+        Transaction t = new Transaction(PARAMS);
+        t.addOutput(new TransactionOutput(PARAMS, t, amount, toKey));
+        t.addSignedInput(spendableOutput, new Script(spendableOutputScriptPubKey), outKey);
+        rollingBlock.addTransaction(t);
+        rollingBlock.solve();
+        blockgraph.add(rollingBlock);
+        totalAmount = totalAmount.add(amount);
+
+        List<UTXO> outputs = store.getOpenTransactionOutputs(Lists.newArrayList(address));
+        assertNotNull(outputs);
+        assertEquals("Wrong Number of Outputs", 1, outputs.size());
+        UTXO output = outputs.get(0);
+        assertEquals("The address is not equal", address.toString(), output.getAddress());
+        assertEquals("The amount is not equal", totalAmount, output.getValue());
+
+        outputs = null;
+        output = null;
+        try {
+            store.close();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void testUTXOProviderWithWallet() throws Exception {
+        final int UNDOABLE_BLOCKS_STORED = 1000;
+        store = createStore(PARAMS, UNDOABLE_BLOCKS_STORED);
+        blockgraph = new FullPrunedBlockGraph(PARAMS, store);
+
+        // Check that we aren't accidentally leaving any references
+        // to the full StoredUndoableBlock's lying around (ie memory leaks)
+        ECKey outKey = new ECKey();
+        int height = 1;
+
+        // Build some blocks on genesis block to create a spendable output.
+        Block rollingBlock = PARAMS.getGenesisBlock().createNextBlockWithCoinbase(Block.BLOCK_VERSION_GENESIS,
+                outKey.getPubKey(), height++, PARAMS.getGenesisBlock().getHash());
+        blockgraph.add(rollingBlock);
+        Transaction transaction = rollingBlock.getTransactions().get(0);
+        TransactionOutPoint spendableOutput = new TransactionOutPoint(PARAMS, 0, transaction.getHash());
+        byte[] spendableOutputScriptPubKey = transaction.getOutputs().get(0).getScriptBytes();
+        for (int i = 1; i < PARAMS.getSpendableCoinbaseDepth(); i++) {
+            rollingBlock = rollingBlock.createNextBlockWithCoinbase(Block.BLOCK_VERSION_GENESIS, outKey.getPubKey(),
+                    height++, PARAMS.getGenesisBlock().getHash());
+            blockgraph.add(rollingBlock);
+        }
+        rollingBlock = rollingBlock.createNextBlock(null, PARAMS.getGenesisBlock().getHash());
+
+        // Create 1 BTC spend to a key in this wallet (to ourselves).
+        // Wallet wallet = new Wallet(PARAMS);
+        // assertEquals("Available balance is incorrect", Coin.ZERO,
+        // wallet.getBalance(Wallet.BalanceType.AVAILABLE));
+        List<byte[]> pubKeyHashs = new ArrayList<byte[]>();
+        pubKeyHashs.add(outKey.getPubKey());
+       Coin coin= transactionService.getBalance(pubKeyHashs);
+       System.out.println(coin.value);
+    }
+
+}
