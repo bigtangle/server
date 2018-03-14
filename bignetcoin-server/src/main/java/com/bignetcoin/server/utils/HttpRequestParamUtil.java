b0eb3284a1eab14493a934f273d475e384ebdf48
姜涛 <jiangtao19890902@sina.com>
2018-03-14 22:07:33 +0800

    Change-Id: I0000000000000000000000000000000000000000

---
+++ bignetcoin-server/src/main/java/com/bignetcoin/server/utils/HttpRequestParamUtil.java
@@ -0,0 +1,23 @@
+package com.bignetcoin.server.utils;
+
+import java.util.List;
+import java.util.Map;
+
+public class HttpRequestParamUtil {
+
+    public static int getParameterAsInt(Map<String, Object> request, String paramName) {
+        int result = ((Integer) request.get(paramName)).intValue();
+        return result;
+    }
+    
+    public static String getParameterAsString(Map<String, Object> request, String paramName) {
+        String result = (String) request.get(paramName);
+        return result;
+    }
+
+    public static List<String> getParameterAsList(Map<String, Object> request, String paramName) {
+        @SuppressWarnings("unchecked")
+        final List<String> paramList = (List<String>) request.get(paramName);
+        return paramList;
+    }
+}
