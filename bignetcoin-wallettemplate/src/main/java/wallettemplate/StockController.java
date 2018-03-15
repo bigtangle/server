6bf58bbb2685879c3a200a3dd41e99aa2da98f29
mi <mi@inasset.de>
2018-03-15 17:49:23 +0800

    publish stock ui

---
+++ bignetcoin-wallettemplate/src/main/java/wallettemplate/StockController.java
@@ -0,0 +1,17 @@
+package wallettemplate;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class StockController {
+    public TextField stockName;
+    public TextField stockCode;
+    public TextField stockNumber;
+    public TextField stockDescription;
+    
+    
+    public void saveStock(ActionEvent event) {
+
+    }
+
+}
