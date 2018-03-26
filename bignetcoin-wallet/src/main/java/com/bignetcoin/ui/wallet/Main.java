b6b7ceeb05e39b5a9dd0448ad73f7a72f05feb2a
mi <mi@inasset.de>
2018-03-26 14:59:14 +0800

    UI

--- bignetcoin-wallet/src/main/java/com/bignetcoin/ui/wallet/Main.java
+++ bignetcoin-wallet/src/main/java/com/bignetcoin/ui/wallet/Main.java
@@ -57,7 +57,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static NetworkParameters params = UnitTestParams.get();
-    public static final String APP_NAME = "WalletTemplate";
+    public static final String APP_NAME = "Wallet";
     private static final String WALLET_FILE_NAME = APP_NAME.replaceAll("[^a-zA-Z0-9.-]", "_") + "-"
             + params.getPaymentProtocolId();
     public static String keyFileDirectory = ".";
