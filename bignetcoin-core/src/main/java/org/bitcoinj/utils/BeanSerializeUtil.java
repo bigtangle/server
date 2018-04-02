8cbb09b78897076036dd79b289f2754503b95281
姜涛\Administrator <jiangtao19890902@sina.com>
2018-04-02 09:45:38 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- bignetcoin-core/src/main/java/org/bitcoinj/utils/BeanSerializeUtil.java
+++ bignetcoin-core/src/main/java/org/bitcoinj/utils/BeanSerializeUtil.java
@@ -7,10 +7,10 @@ import java.io.ObjectOutputStream;
 
 public class BeanSerializeUtil {
 
-    public static <T> byte[] serializer(T obj) throws Exception {
+    public static <T> byte[] serializer(T object) throws Exception {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-        objectOutputStream.writeObject(obj);
+        objectOutputStream.writeObject(object);
         byte[] buf = byteArrayOutputStream.toByteArray();
         return buf;
     }
@@ -18,7 +18,7 @@ public class BeanSerializeUtil {
     public static <T> T deserialize(byte[] buf, Class<T> clazz) throws Exception {
         ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(buf));
         @SuppressWarnings("unchecked")
-        T obj = (T) objectInputStream.readObject();
-        return obj;
+        T object = (T) objectInputStream.readObject();
+        return object;
     }
 }
