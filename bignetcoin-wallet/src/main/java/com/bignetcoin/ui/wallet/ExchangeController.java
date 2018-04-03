f715a180e0732bb84951ab512f0f31245510ad34
mi <mi@inasset.de>
2018-04-03 17:45:21 +0800

    token combobox init

--- bignetcoin-wallet/src/main/java/com/bignetcoin/ui/wallet/ExchangeController.java
+++ bignetcoin-wallet/src/main/java/com/bignetcoin/ui/wallet/ExchangeController.java
@@ -11,10 +11,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Json;
+import org.bitcoinj.utils.OkHttp3Util;
 
 import com.bignetcoin.ui.wallet.utils.GuiUtils;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
@@ -22,11 +32,46 @@ public class ExchangeController {
     public TextField stockAddress;
     public TextField stockCode;
     public TextField stockNumber;
+    @FXML
+    public ComboBox<String> toTokenHexComboBox;
+    @FXML
+    public ComboBox<String> toAddressComboBox;
 
     public TextField coinAddress;
     public TextField coinAmount;
     public TextField coinTokenid;
+    @FXML
+    public ComboBox<String> fromTokenHexComboBox;
+    @FXML
+    public ComboBox<String> fromAddressComboBox;
     public Main.OverlayUI overlayUI;
+
+    @FXML
+    public void initialize() {
+        try {
+            initComboBox();
+        } catch (Exception e) {
+            GuiUtils.crashAlert(e);
+        }
+    }
+
+    public void initComboBox() throws Exception {
+        String CONTEXT_ROOT = "http://" + Main.IpAddress + ":" + Main.port + "/";
+        ObservableList<String> tokenData = FXCollections.observableArrayList();
+        ECKey ecKey = Main.bitcoin.wallet().currentReceiveKey();
+        String response = OkHttp3Util.post(CONTEXT_ROOT + "getTokens", ecKey.getPubKeyHash());
+
+        final Map<String, Object> data = Json.jsonmapper().readValue(response, Map.class);
+
+        List<Map<String, Object>> list = (List<Map<String, Object>>) data.get("tokens");
+        for (Map<String, Object> map : list) {
+            String tokenHex = (String) map.get("tokenHex");
+            tokenData.add(tokenHex);
+        }
+        toTokenHexComboBox.setItems(tokenData);
+        fromTokenHexComboBox.setItems(tokenData);
+
+    }
 
     public void exchangeCoin(ActionEvent event) {
 
