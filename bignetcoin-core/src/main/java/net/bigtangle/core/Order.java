2886e0b9befd93fe109bf32c35d059650d5e4f0a
姜涛 <jiangtao19890902@gmail.com>
2018-04-08 14:41:23 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- bignetcoin-core/src/main/java/net/bigtangle/core/Order.java
+++ bignetcoin-core/src/main/java/net/bigtangle/core/Order.java
@@ -1,8 +1,22 @@
 package net.bigtangle.core;
 
+import java.security.KeyStore.PrivateKeyEntry;
+
 public class Order implements java.io.Serializable {
 
     private static final long serialVersionUID = 190060684620430983L;
     
     private String orderid;
+    
+    private String address;
+    
+    private String tokenid;
+    
+    private int type;
+    
+    private String validateto;
+    
+    private String validatefrom;
+    
+    private int state;
 }
