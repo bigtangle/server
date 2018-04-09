cda7338ff84b3aeaf07e695c19c5e21ef359a0de
姜涛 <jiangtao19890902@sina.com>
2018-04-09 20:18:46 +0800

    save order

--- bignetcoin-wallet/src/main/java/net/bigtangle/ui/wallet/OrderController.java
+++ bignetcoin-wallet/src/main/java/net/bigtangle/ui/wallet/OrderController.java
@@ -5,6 +5,7 @@
 package net.bigtangle.ui.wallet;
 
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,7 +36,7 @@ public class OrderController {
     @FXML
     public DatePicker validdateToDatePicker;
 
-    public Main.OverlayUI overlayUI;
+    public Main.OverlayUI<?> overlayUI;
 
     @FXML
     public void initialize() {
@@ -46,14 +47,13 @@ public class OrderController {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public void initComboBox() throws Exception {
         String CONTEXT_ROOT = "http://" + Main.IpAddress + ":" + Main.port + "/";
         ObservableList<String> tokenData = FXCollections.observableArrayList();
         ECKey ecKey = Main.bitcoin.wallet().currentReceiveKey();
         String response = OkHttp3Util.post(CONTEXT_ROOT + "getTokens", ecKey.getPubKeyHash());
-
         final Map<String, Object> data = Json.jsonmapper().readValue(response, Map.class);
-
         List<Map<String, Object>> list = (List<Map<String, Object>>) data.get("tokens");
         for (Map<String, Object> map : list) {
             String tokenHex = (String) map.get("tokenHex");
@@ -61,22 +61,39 @@ public class OrderController {
             tokenData.add(tokenname + " : " + tokenHex);
         }
         tokenComboBox.setItems(tokenData);
-
         List<ECKey> keys = Main.bitcoin.wallet().walletKeys(null);
         ObservableList<String> addresses = FXCollections.observableArrayList();
         for (ECKey key : keys) {
             addresses.add(key.toAddress(Main.params).toString());
         }
         addressComboBox.setItems(addresses);
-
         ObservableList<Object> statusData = FXCollections.observableArrayList("buy", "sell");
         statusChoiceBox.setItems(statusData);
     }
 
     public void buy(ActionEvent event) throws Exception {
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        if (validdateFromDatePicker.getValue().equals("")) {
+            GuiUtils.informationalAlert("save order param", "validdate From Date Picker ERROR");
+            return;
+        }
+        if (validdateToDatePicker.getValue().equals("")) {
+            GuiUtils.informationalAlert("save order param", "validdate To Date Picker ERROR");
+            return;
+        }
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd 00:00:00");
         String validdateFrom = df.format(validdateFromDatePicker.getValue());
         String validdateTo = df.format(validdateToDatePicker.getValue());
+        String ContextRoot = "http://" + Main.IpAddress + ":" + Main.port + "/";
+        HashMap<String, Object> requestParam = new HashMap<String, Object>();
+        requestParam.put("address", this.addressComboBox.getValue());
+        requestParam.put("tokenid", this.tokenComboBox.getValue());
+        String typeStr = (String) statusChoiceBox.getValue();
+        requestParam.put("type", typeStr.equals("sell") ? 1 : 0);
+        int limit = Integer.parseInt(this.limitTextField.getText());
+        requestParam.put("limitl", limit);
+        requestParam.put("validateto", validdateTo);
+        requestParam.put("validatefrom", validdateFrom);
+        OkHttp3Util.post(ContextRoot + "saveOrder", Json.jsonmapper().writeValueAsString(requestParam));
         overlayUI.done();
     }
 
