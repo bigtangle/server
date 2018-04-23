836a1ec0e85b717020e56e63717ef90319c0e804
mi <mi@inasset.de>
2018-04-23 15:50:11 +0800

    search block

--- bignetcoin-wallet/src/main/java/net/bigtangle/ui/wallet/BlockEvaluationController.java
+++ bignetcoin-wallet/src/main/java/net/bigtangle/ui/wallet/BlockEvaluationController.java
@@ -1,7 +1,5 @@
 package net.bigtangle.ui.wallet;
 
-import static net.bigtangle.ui.wallet.Main.bitcoin;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +9,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.MapValueFactory;
@@ -35,11 +34,22 @@ public class BlockEvaluationController {
     @FXML
     public TableColumn<Map, Number> heightColumn;
 
+    @FXML
+    public ComboBox<String> addressComboBox;
+
     public Main.OverlayUI<?> overlayUI;
 
     @FXML
     public void initialize() {
 
+        try {
+            initTableView();
+        } catch (Exception e) {
+            GuiUtils.crashAlert(e);
+        }
+    }
+
+    public void searchBlock(ActionEvent event) {
         try {
             initTableView();
         } catch (Exception e) {
@@ -54,32 +64,16 @@ public class BlockEvaluationController {
     @SuppressWarnings("unchecked")
     public void initTableView() throws Exception {
         String CONTEXT_ROOT = "http://" + Main.IpAddress + ":" + Main.port + "/";
-        String response = OkHttp3Util.post(CONTEXT_ROOT + "getAllEvaluations",
-                bitcoin.wallet().currentReceiveKey().getPubKeyHash());
+
+        String address = addressComboBox.getValue();
+        Map<String, String> requestParam = new HashMap<String, String>();
+        requestParam.put("address", address);
+        String response = OkHttp3Util.post(CONTEXT_ROOT + "searchBlock",
+                Json.jsonmapper().writeValueAsString(requestParam).getBytes());
         final Map<String, Object> data = Json.jsonmapper().readValue(response, Map.class);
         List<Map<String, Object>> temp = (List<Map<String, Object>>) data.get("evaluations");
-      /*  if (temp != null && !temp.isEmpty()) {
-            ObservableList<Map> allData = FXCollections.observableArrayList();
-            for (Map<String, Object> evaluationMap : temp) {
-                Map<String, Object> dataRow = new HashMap<>();
-                dataRow.put("hash", ((Map<String, String>) evaluationMap.get("blockhash")).get("bytes"));
-                dataRow.put("rating", evaluationMap.get("rating"));
-                dataRow.put("depth", evaluationMap.get("depth"));
-                dataRow.put("cumulativeWeight", evaluationMap.get("cumulativeWeight"));
-                dataRow.put("height", evaluationMap.get("height"));
-                allData.add(dataRow);
-            }
-            blockhashColumn.setCellValueFactory(new MapValueFactory("hash"));
-            ratingColumn.setCellValueFactory(new MapValueFactory("rating"));
-            depthColumn.setCellValueFactory(new MapValueFactory("depth"));
-            cumulativeWeightColumn.setCellValueFactory(new MapValueFactory("cumulativeWeight"));
-            heightColumn.setCellValueFactory(new MapValueFactory("height"));
 
-            blockhashColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-            blockEvaluationTable.setItems(allData);
-        }*/
-      List<BlockEvaluation> list = temp.stream().map(map -> MapToBeanMapperUtil.parseBlockEvaluation(map))
+        List<BlockEvaluation> list = temp.stream().map(map -> MapToBeanMapperUtil.parseBlockEvaluation(map))
                 .collect(Collectors.toList());
         if (list != null && !list.isEmpty()) {
             ObservableList<Map> allData = FXCollections.observableArrayList();
