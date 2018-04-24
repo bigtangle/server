4e82b82a1bc9c08ec3ed4849375e8a73f8191a06
mi <mi@inasset.de>
2018-04-24 16:19:57 +0800

    send multiSign

--- bignetcoin-wallet/src/main/java/net/bigtangle/ui/wallet/SendMoneyController.java
+++ bignetcoin-wallet/src/main/java/net/bigtangle/ui/wallet/SendMoneyController.java
@@ -21,10 +21,15 @@ package net.bigtangle.ui.wallet;
 import static com.google.common.base.Preconditions.checkState;
 import static net.bigtangle.ui.wallet.utils.GuiUtils.checkGuiThread;
 
+import java.io.File;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.spongycastle.crypto.params.KeyParameter;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -36,20 +41,28 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.FileChooser;
 import net.bigtangle.core.Address;
 import net.bigtangle.core.Block;
 import net.bigtangle.core.Coin;
 import net.bigtangle.core.ECKey;
 import net.bigtangle.core.Json;
 import net.bigtangle.core.NetworkParameters;
+import net.bigtangle.core.Transaction;
+import net.bigtangle.core.TransactionOutput;
+import net.bigtangle.core.UTXO;
 import net.bigtangle.core.Utils;
-import net.bigtangle.crypto.DeterministicKey;
+import net.bigtangle.crypto.KeyCrypterScrypt;
+import net.bigtangle.ui.wallet.utils.FileUtil;
 import net.bigtangle.ui.wallet.utils.GuiUtils;
 import net.bigtangle.ui.wallet.utils.TextFieldValidator;
 import net.bigtangle.ui.wallet.utils.WTUtils;
+import net.bigtangle.utils.MapToBeanMapperUtil;
 import net.bigtangle.utils.OkHttp3Util;
+import net.bigtangle.utils.UUIDUtil;
 import net.bigtangle.wallet.SendRequest;
 import net.bigtangle.wallet.Wallet;
+import net.bigtangle.wallet.Wallet.MissingSigsMode;
 
 public class SendMoneyController {
     public Button sendBtn;
@@ -72,6 +85,9 @@ public class SendMoneyController {
     public ChoiceBox<Object> tokeninfo;
 
     public Main.OverlayUI<?> overlayUI;
+
+    private String mOrderid;
+    private Transaction mTransaction;
 
     public void initChoicebox() {
         basicRadioButton.setUserData(1 + "");
@@ -106,17 +122,170 @@ public class SendMoneyController {
     @FXML
     public void initialize() throws Exception {
         initChoicebox();
-      //  DeterministicKey ecKey = Main.bitcoin.wallet().currentReceiveKey();
-      //  address.setText(ecKey.toAddress(Main.params).toBase58());
+        // DeterministicKey ecKey = Main.bitcoin.wallet().currentReceiveKey();
+        // address.setText(ecKey.toAddress(Main.params).toBase58());
         new TextFieldValidator(amountEdit, text -> !WTUtils
                 .didThrow(() -> checkState(Coin.parseCoin(text, NetworkParameters.BIGNETCOIN_TOKENID).isPositive())));
 
     }
 
     public void importSign(ActionEvent event) {
+        final FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(null);
+        byte[] buf = FileUtil.readFile(file);
+        if (buf == null) {
+            return;
+        }
+        reloadTransaction(buf);
+        overlayUI.done();
+    }
+
+    private void reloadTransaction(byte[] buf) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(buf);
+
+        {
+            byte[] dst = new byte[byteBuffer.getInt()];
+            byteBuffer.get(dst);
+            address.setText(new String(dst));
+        }
+        {
+            byte[] dst = new byte[byteBuffer.getInt()];
+            byteBuffer.get(dst);
+            tokeninfo.setValue(new String(dst));
+        }
+        {
+            byte[] dst = new byte[byteBuffer.getInt()];
+            byteBuffer.get(dst);
+            amountEdit.setText(new String(dst));
+        }
+        byte[] orderid = new byte[byteBuffer.getInt()];
+        byteBuffer.get(orderid);
+
+        mOrderid = new String(orderid);
+        // System.out.println("orderid : " + new String(orderid));
+
+        int len = byteBuffer.getInt();
+        // System.out.println("tx len : " + len);
+        byte[] data = new byte[len];
+        byteBuffer.get(data);
+        try {
+            mTransaction = (Transaction) Main.params.getDefaultSerializer().makeTransaction(data);
+            // mTransaction = (Transaction)
+            // Main.params.getDefaultSerializer().makeTransaction(data);
+            if (mTransaction == null) {
+                GuiUtils.informationalAlert("alert", "Transaction Is Empty");
+            }
+        } catch (Exception e) {
+            GuiUtils.crashAlert(e);
+        }
     }
 
     public void exportSign(ActionEvent event) {
+        String toAddress = address.getText();
+        String toTokenHex = tokeninfo.getValue().toString().split(":")[1].trim();
+        String toAmount = amountEdit.getText();
+        this.mOrderid = UUIDUtil.randomUUID();
+        byte[] buf = this.makeSignTransactionBuffer(toAddress, getCoin(toAmount, toTokenHex, false));
+        if (buf == null) {
+            return;
+        }
+
+        final FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showSaveDialog(null);
+        if (file == null) {
+            return;
+        }
+
+        FileUtil.writeFile(file, buf);
+        overlayUI.done();
+    }
+
+    public Coin getCoin(String toAmount, String toTokenHex, boolean decimal) {
+        if (decimal) {
+            return Coin.parseCoin(toAmount, Utils.HEX.decode(toTokenHex));
+        } else {
+            return Coin.valueOf(Long.parseLong(toAmount), Utils.HEX.decode(toTokenHex));
+        }
+    }
+
+    private byte[] makeSignTransactionBuffer(String toAddress, Coin toCoin) {
+        String ContextRoot = "http://" + Main.IpAddress + ":" + Main.port + "/";
+        Address toAddress00 = new Address(Main.params, toAddress);
+        KeyParameter aesKey = null;
+        // Main.initAeskey(aesKey);
+        final KeyCrypterScrypt keyCrypter = (KeyCrypterScrypt) Main.bitcoin.wallet().getKeyCrypter();
+        if (!"".equals(Main.password.trim())) {
+            aesKey = keyCrypter.deriveKey(Main.password);
+        }
+        byte[] buf = null;
+        try {
+            List<UTXO> outputs = new ArrayList<UTXO>();
+            outputs.addAll(Main.getUTXOWithPubKeyHash(toAddress00.getHash160(), null));
+            outputs.addAll(this.getUTXOWithECKeyList(Main.bitcoin.wallet().walletKeys(aesKey),
+                    Utils.HEX.decode(toCoin.getTokenHex())));
+
+            SendRequest req = SendRequest.to(toAddress00, toCoin);
+
+            req.missingSigsMode = MissingSigsMode.USE_OP_ZERO;
+
+            HashMap<String, Address> addressResult = new HashMap<String, Address>();
+            addressResult.put(toCoin.getTokenHex(), toAddress00);
+
+            List<TransactionOutput> candidates = Main.bitcoin.wallet().transforSpendCandidates(outputs);
+            Main.bitcoin.wallet().setServerURL(ContextRoot);
+            Main.bitcoin.wallet().completeTx(req, candidates, false, addressResult);
+            Main.bitcoin.wallet().signTransaction(req);
+
+            this.mTransaction = req.tx;
+            buf = mTransaction.bitcoinSerialize();
+        } catch (Exception e) {
+            GuiUtils.crashAlert(e);
+            return null;
+        }
+        return makeSignTransactionBuffer(toAddress, toCoin, buf);
+    }
+
+    private byte[] makeSignTransactionBuffer(String toAddress, Coin toCoin, byte[] buf) {
+        ByteBuffer byteBuffer = ByteBuffer
+                .allocate(buf.length + 4 + toAddress.getBytes().length + 4 + toCoin.getTokenHex().getBytes().length + 4
+                        + toCoin.toPlainString().getBytes().length + 4 + this.mOrderid.getBytes().length + 4);
+
+        // byteBuffer.putInt(fromAddress.getBytes().length).put(fromAddress.getBytes());
+        // byteBuffer.putInt(fromCoin.getTokenHex().getBytes().length).put(fromCoin.getTokenHex().getBytes());
+        // byteBuffer.putInt(fromCoin.toPlainString().getBytes().length).put(fromCoin.toPlainString().getBytes());
+        byteBuffer.putInt(toAddress.getBytes().length).put(toAddress.getBytes());
+        byteBuffer.putInt(toCoin.getTokenHex().getBytes().length).put(toCoin.getTokenHex().getBytes());
+        byteBuffer.putInt(toCoin.toPlainString().getBytes().length).put(toCoin.toPlainString().getBytes());
+        byteBuffer.putInt(this.mOrderid.getBytes().length).put(this.mOrderid.getBytes());
+        byteBuffer.putInt(buf.length).put(buf);
+        // System.out.println("tx len : " + buf.length);
+        return byteBuffer.array();
+    }
+
+    public List<UTXO> getUTXOWithECKeyList(List<ECKey> ecKeys, byte[] tokenid) throws Exception {
+        List<UTXO> listUTXO = new ArrayList<UTXO>();
+        String ContextRoot = "http://" + Main.IpAddress + ":" + Main.port + "/";
+        for (ECKey ecKey : ecKeys) {
+            String response = OkHttp3Util.post(ContextRoot + "getOutputs", ecKey.getPubKeyHash());
+            final Map<String, Object> data = Json.jsonmapper().readValue(response, Map.class);
+            if (data == null || data.isEmpty()) {
+                return listUTXO;
+            }
+            List<Map<String, Object>> outputs = (List<Map<String, Object>>) data.get("outputs");
+            if (outputs == null || outputs.isEmpty()) {
+                return listUTXO;
+            }
+            for (Map<String, Object> object : outputs) {
+                UTXO utxo = MapToBeanMapperUtil.parseUTXO(object);
+                if (!Arrays.equals(utxo.getTokenid(), tokenid)) {
+                    continue;
+                }
+                if (utxo.getValue().getValue() > 0) {
+                    listUTXO.add(utxo);
+                }
+            }
+        }
+        return listUTXO;
     }
 
     public void cancel(ActionEvent event) {
