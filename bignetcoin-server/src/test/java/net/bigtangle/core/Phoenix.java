a453f8248c24bb971fcedde8294c335ecb9eca9e
姜涛 <jiangtao19890902@sina.com>
2018-04-24 13:35:22 +0800

    1

---
+++ bignetcoin-server/src/test/java/net/bigtangle/core/Phoenix.java
@@ -0,0 +1,34 @@
+package net.bigtangle.core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Phoenix {
+
+    private static String driver = "org.apache.phoenix.jdbc.PhoenixDriver";
+
+    public static void main(String[] args) throws SQLException {
+        System.setProperty("hadoop.home.dir", "D:\\environment\\hadoop-2.6.3");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        Connection con = DriverManager.getConnection("jdbc:phoenix:cn.phoenix.bigtangle.net:8765");
+        stmt = con.createStatement();
+        String sql = "select * from test";
+        rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.print("id:" + rs.getString("id"));
+            System.out.println(",name:" + rs.getString("name"));
+        }
+        stmt.close();
+        con.close();
+    }
+}
