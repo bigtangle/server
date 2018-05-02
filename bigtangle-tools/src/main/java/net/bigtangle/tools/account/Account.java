60b5ea6edf55690950df933820502e0c39bf73c7
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-02 10:18:00 +0800

    1

--- bigtangle-tools/src/main/java/net/bigtangle/tools/account/Account.java
+++ bigtangle-tools/src/main/java/net/bigtangle/tools/account/Account.java
@@ -3,16 +3,19 @@ package net.bigtangle.tools.account;
 import java.io.File;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import net.bigtangle.core.ECKey;
-import net.bigtangle.core.NetworkParameters;
 import net.bigtangle.kits.WalletAppKit;
-import net.bigtangle.params.UnitTestParams;
 import net.bigtangle.tools.action.Action;
 import net.bigtangle.tools.action.impl.BalancesAction;
+import net.bigtangle.tools.action.impl.BuyOrderAction;
 import net.bigtangle.tools.action.impl.PayAction;
-import net.bigtangle.tools.config.Tools;
+import net.bigtangle.tools.action.impl.SellOrderAction;
+import net.bigtangle.tools.action.impl.TransferAction;
+import net.bigtangle.tools.config.Configure;
 import net.bigtangle.tools.thread.TradeRun;
+import net.bigtangle.wallet.SendRequest;
 
 import org.spongycastle.crypto.params.KeyParameter;
 
@@ -20,7 +23,7 @@ public class Account {
 
     private HashMap<String, Action> executes = new HashMap<String, Action>();
 
-    private final static NetworkParameters PARAMS = UnitTestParams.get();
+    private Random random = new Random();
 
     public List<ECKey> walletKeys() throws Exception {
         List<ECKey> walletKeys = walletAppKit.wallet().walletKeys(aesKey);
@@ -40,12 +43,18 @@ public class Account {
 
     public void initialize() {
         // init wallet
-        walletAppKit = new WalletAppKit(PARAMS, new File("."), walletPath);
-        walletAppKit.wallet().setServerURL(Tools.CONTEXT_ROOT);
+        walletAppKit = new WalletAppKit(Configure.PARAMS, new File("."), walletPath);
+        walletAppKit.wallet().setServerURL(Configure.CONTEXT_ROOT);
 
+        // give amount
         Action action = new PayAction(this);
         action.execute();
+
+        // init action
         this.executes.put(BalancesAction.class.getSimpleName(), new BalancesAction(this));
+        this.executes.put(BuyOrderAction.class.getSimpleName(), new BuyOrderAction(this));
+        this.executes.put(SellOrderAction.class.getSimpleName(), new SellOrderAction(this));
+        this.executes.put(TransferAction.class.getSimpleName(), new TransferAction(this));
     }
 
     public void doAction() {
@@ -61,6 +70,23 @@ public class Account {
     }
 
     public Random0 getRandom0() {
-        return null;
+        return new Random0("", "");
+    }
+
+    public ECKey getRandomECKey() {
+        List<ECKey> walletKeys = null;
+        try {
+            walletKeys = this.walletKeys();
+        } catch (Exception e) {
+        }
+        if (walletKeys == null || walletKeys.isEmpty()) {
+            return null;
+        }
+        int index = random.nextInt(walletKeys.size());
+        return walletKeys.get(index);
+    }
+
+    public void completeTransaction(SendRequest request) throws Exception {
+        this.walletAppKit.wallet().completeTx(request);
     }
 }
