9578cce4953ce44443fff9e72b257771c378f889
姜涛\Administrator <jiangtao19890902@sina.com>
2018-05-02 17:20:53 +0800

    1

---
+++ bigtangle-tools/src/main/java/net/bigtangle/tools/container/TokenPost.java
@@ -0,0 +1,34 @@
+package net.bigtangle.tools.container;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.bigtangle.core.Json;
+import net.bigtangle.tools.config.Configure;
+import net.bigtangle.utils.OkHttp3Util;
+
+public class TokenPost extends ArrayList<String> {
+
+    private static final long serialVersionUID = -8299620590125212324L;
+    
+    private static final TokenPost instance = new TokenPost();
+    
+    public static TokenPost getInstance() {
+        return instance;
+    }
+    
+    public void initialize() throws Exception {
+        HashMap<String, Object> requestParam = new HashMap<String, Object>();
+        String response = OkHttp3Util.post(Configure.CONTEXT_ROOT + "getTokens", Json.jsonmapper().writeValueAsString(requestParam).getBytes());
+        @SuppressWarnings("unchecked")
+        final Map<String, Object> data = Json.jsonmapper().readValue(response, Map.class);
+        @SuppressWarnings("unchecked")
+        List<Map<String, Object>> list = (List<Map<String, Object>>) data.get("tokens");
+        for (Map<String, Object> map : list) {
+            String tokenHex = (String) map.get("tokenHex");
+            this.add(tokenHex);
+        }
+    }
+}
