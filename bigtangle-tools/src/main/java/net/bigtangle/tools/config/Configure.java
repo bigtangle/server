fbd9ed65bef042cdbc8ed2f271b25c590f6bcc4b
姜涛 <jiangtao19890902@sina.com>
2018-05-07 00:33:58 +0800

    1

--- bigtangle-tools/src/main/java/net/bigtangle/tools/config/Configure.java
+++ bigtangle-tools/src/main/java/net/bigtangle/tools/config/Configure.java
@@ -1,5 +1,6 @@
 package net.bigtangle.tools.config;
 
+import net.bigtangle.core.ECKey;
 import net.bigtangle.core.NetworkParameters;
 import net.bigtangle.params.UnitTestParams;
 
@@ -8,4 +9,6 @@ public class Configure {
     public final static String CONTEXT_ROOT = "http://localhost:8088/";
     
     public final static NetworkParameters PARAMS = UnitTestParams.get();
+    
+    public final static ECKey OUT_KEY = new ECKey();
 }
