cbcc6e142a5cc2c6978e8adaa644c228f1723419
cui <cui@inasset.de>
2018-05-12 18:04:46 +0200

    add spark

---
+++ bigtangle-spark/src/test/scala/de/gd/analytics/test/JdbcTest.scala
@@ -0,0 +1,36 @@
+package de.gd.analytics.test
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructField
+
+import com.typesafe.config.ConfigFactory
+
+object JdbcTest {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[4]").setAppName("FirstJob")
+    val sc = new SparkContext(conf)
+    val config = ConfigFactory.parseString("")
+    System.setProperty("HADOOP_USER_NAME", "hdfs");
+    val sql = new SQLContext(sc)
+
+    val tabledata = "{\"financial_year\":\"2004-05\",\"state\":\"TAS\",\"area_of_expenditure\":\"Community health\",\"broad_source_of_funding\":\"Government\",\"detailed_source_of_funding\":\"Australian Government\",\"real_expenditure_millions\":\"13\"}"
+
+    Class.forName("com.mysql.jdbc.Driver").newInstance
+
+    val sqlContext = new SQLContext(sc)
+
+    val data = sqlContext.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306/info")
+      .option("user", "root")
+      .option("password", "test1234")
+      .option("dbtable", "headers")
+      .load()
+      data.printSchema()
+  }
+
+}
