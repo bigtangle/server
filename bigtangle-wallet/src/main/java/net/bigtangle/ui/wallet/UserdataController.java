3f829ee839d015bbf4e6497195308aab528c4ce6
mi <mi@inasset.de>
2018-06-12 13:17:11 +0800

    UI

--- bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/UserdataController.java
+++ bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/UserdataController.java
@@ -4,6 +4,7 @@
  *******************************************************************************/
 package net.bigtangle.ui.wallet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +22,9 @@ import net.bigtangle.core.ContactInfo;
 import net.bigtangle.core.DataClassName;
 import net.bigtangle.core.ECKey;
 import net.bigtangle.core.Json;
+import net.bigtangle.core.MultiSignBy;
 import net.bigtangle.core.NetworkParameters;
+import net.bigtangle.core.Sha256Hash;
 import net.bigtangle.core.Transaction;
 import net.bigtangle.core.Utils;
 import net.bigtangle.ui.wallet.utils.GuiUtils;
@@ -62,29 +65,7 @@ public class UserdataController {
     public void saveUserdata(ActionEvent event) {
         String CONTEXT_ROOT = "http://" + Main.IpAddress + ":" + Main.port + "/";
         try {
-            HashMap<String, String> requestParam = new HashMap<String, String>();
-            byte[] data = OkHttp3Util.post(CONTEXT_ROOT + "askTransaction",
-                    Json.jsonmapper().writeValueAsString(requestParam));
-            Block block = Main.params.getDefaultSerializer().makeBlock(data);
-            block.setBlocktype( NetworkParameters.BLOCKTYPE_USERDATA);
-
-            Transaction coinbase = new Transaction(Main.params);
-            Contact contact = new Contact();
-            contact.setName(nameTF.getText());
-            contact.setAddress(addressTF.getText());
-            ContactInfo contactInfo = getUserdata();
-            List<Contact> list = contactInfo.getContactList();
-            list.add(contact);
-            contactInfo.setContactList(list);
-
-            coinbase.setDataclassname(DataClassName.USERDATA.name());
-            byte[] buf1 = contactInfo.toByteArray();
-            coinbase.setData(buf1);
-
-            block.addTransaction(coinbase);
-            block.solve();
-
-            OkHttp3Util.post(CONTEXT_ROOT + "saveBlock", block.bitcoinSerialize());
+            addContact(CONTEXT_ROOT);
             initContactTableView();
         } catch (Exception e) {
             GuiUtils.crashAlert(e);
@@ -104,6 +85,44 @@ public class UserdataController {
 
     }
 
+    public void addContact(String contextRoot) throws Exception {
+        HashMap<String, String> requestParam = new HashMap<String, String>();
+        byte[] data = OkHttp3Util.post(contextRoot + "askTransaction",
+                Json.jsonmapper().writeValueAsString(requestParam));
+        Block block = Main.params.getDefaultSerializer().makeBlock(data);
+        block.setBlocktype(NetworkParameters.BLOCKTYPE_USERDATA);
+        ECKey pubKeyTo = Main.bitcoin.wallet().currentReceiveKey();
+
+        Transaction coinbase = new Transaction(Main.params);
+        Contact contact = new Contact();
+        contact.setName(nameTF.getText());
+        contact.setAddress(addressTF.getText());
+        ContactInfo contactInfo = getUserdata();
+        List<Contact> list = contactInfo.getContactList();
+        list.add(contact);
+        contactInfo.setContactList(list);
+
+        coinbase.setDataclassname(DataClassName.USERDATA.name());
+        coinbase.setData(contactInfo.toByteArray());
+
+        Sha256Hash sighash = coinbase.getHash();
+        ECKey.ECDSASignature party1Signature = pubKeyTo.sign(sighash);
+        byte[] buf1 = party1Signature.encodeToDER();
+
+        List<MultiSignBy> multiSignBies = new ArrayList<MultiSignBy>();
+        MultiSignBy multiSignBy0 = new MultiSignBy();
+        multiSignBy0.setAddress(pubKeyTo.toAddress(Main.params).toBase58());
+        multiSignBy0.setPublickey(Utils.HEX.encode(pubKeyTo.getPubKey()));
+        multiSignBy0.setSignature(Utils.HEX.encode(buf1));
+        multiSignBies.add(multiSignBy0);
+        coinbase.setDatasignature(Json.jsonmapper().writeValueAsBytes(multiSignBies));
+
+        block.addTransaction(coinbase);
+        block.solve();
+
+        OkHttp3Util.post(contextRoot + "saveBlock", block.bitcoinSerialize());
+    }
+
     public void closeUI(ActionEvent event) {
         overlayUI.done();
     }
@@ -112,6 +131,46 @@ public class UserdataController {
     }
 
     public void removeLinkman(ActionEvent event) {
+        try {
+            Map<String, Object> rowdata = linkmanTableview.getSelectionModel().getSelectedItem();
+            if (rowdata == null) {
+                return;
+            }
+            String name = (String) rowdata.get("name");
+            String address = (String) rowdata.get("address");
+            String CONTEXT_ROOT = "http://" + Main.IpAddress + ":" + Main.port + "/";
+            HashMap<String, String> requestParam = new HashMap<String, String>();
+
+            byte[] data = OkHttp3Util.post(CONTEXT_ROOT + "askTransaction",
+                    Json.jsonmapper().writeValueAsString(requestParam));
+
+            Block block = Main.params.getDefaultSerializer().makeBlock(data);
+            block.setBlocktype(NetworkParameters.BLOCKTYPE_USERDATA);
+
+            Transaction coinbase = new Transaction(Main.params);
+
+            ContactInfo contactInfo = getUserdata();
+            List<Contact> list = contactInfo.getContactList();
+            for (Contact contact : list) {
+                if (name.trim().equals(contact.getName().trim())
+                        && address.trim().equals(contact.getAddress().trim())) {
+                    list.remove(contact);
+                }
+            }
+            contactInfo.setContactList(list);
+
+            coinbase.setDataclassname(DataClassName.USERDATA.name());
+            byte[] buf1 = contactInfo.toByteArray();
+            coinbase.setData(buf1);
+
+            block.addTransaction(coinbase);
+            block.solve();
+
+            OkHttp3Util.post(CONTEXT_ROOT + "saveBlock", block.bitcoinSerialize());
+            initContactTableView();
+        } catch (Exception e) {
+            GuiUtils.crashAlert(e);
+        }
     }
 
     public void initTableView() throws Exception {
