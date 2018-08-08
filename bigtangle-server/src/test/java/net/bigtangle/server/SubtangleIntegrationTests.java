07cac8fb0afe17db50290a97cc2495566d6a9c00
姜涛 <jiangtao19890902@gmail.com>
2018-08-08 23:00:41 +0800

    Change-Id: I0000000000000000000000000000000000000000

--- bigtangle-server/src/test/java/net/bigtangle/server/SubtangleIntegrationTests.java
+++ bigtangle-server/src/test/java/net/bigtangle/server/SubtangleIntegrationTests.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.cassandra.utils.Hex;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
