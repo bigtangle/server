a4305b12a7cecb1dfb5730e5d937a1ef52b9f521
cui <cui@inasset.de>
2018-08-10 09:18:27 +0200

    version 0.3.0

--- bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/Main.java
+++ bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/Main.java
@@ -104,8 +104,8 @@ import net.bigtangle.wallet.DeterministicSeed;
 
 public class Main extends Application {
     public static NetworkParameters params = UnitTestParams.get();
-    public static final String APP_NAME = "Bigtangle  Wallet " + Main.version;
-    public static final String version = "0.2.0";
+    public static final String APP_NAME = "Bigtangle  Wallet Test " + Main.version;
+    public static final String version = "0.3.0";
     public static String keyFileDirectory = ".";
     public static String keyFilePrefix = "bigtangle";
     public static WalletAppKit bitcoin;
