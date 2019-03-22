f0fe52ce8a67a392bcc3b910e61b3265bb8803a8
c263 <c263@WKS15006393.localnet.de>
2019-03-22 14:45:27 +0100

    add date for wallet display

--- bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/Main.java
+++ bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/Main.java
@@ -99,7 +99,7 @@ import net.bigtangle.wallet.DeterministicSeed;
 
 public class Main extends Application {
     public static NetworkParameters params = MainNetParams.get();
-    public static final String APP_NAME = "Bigtangle  Wallet Test " + Main.version;
+    public static final String APP_NAME = "Bigtangle  Wallet Test " + Main.version + "-22-03";
     public static final String version = "0.3.3";
     public static String keyFileDirectory = ".";
     public static String keyFilePrefix = "bigtangle";
