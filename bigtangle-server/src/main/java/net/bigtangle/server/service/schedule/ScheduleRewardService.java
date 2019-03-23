71906a15f8b93a5ea189cfc77f6712824770e3e6
cui <cui@inasset.de>
2019-03-23 21:31:28 +0100

    reward to 5 minute

--- bigtangle-server/src/main/java/net/bigtangle/server/service/schedule/ScheduleRewardService.java
+++ bigtangle-server/src/main/java/net/bigtangle/server/service/schedule/ScheduleRewardService.java
@@ -25,7 +25,7 @@ public class ScheduleRewardService {
     @Autowired
     private  TransactionService transactionService;
 
-    @Scheduled(fixedDelay = 90000)
+    @Scheduled(fixedDelay = 300000)
     public void updateReward() {
         if (scheduleConfiguration.isMilestone_active()) {
             try {
