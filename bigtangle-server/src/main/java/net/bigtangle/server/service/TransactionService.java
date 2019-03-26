d0962aed89348684af99e299a3fff0cc932abd8e
Kai Cui <kai.cui@stud.tu-darmstadt.de>
2019-03-27 00:16:22 +0100

    null check

--- bigtangle-server/src/main/java/net/bigtangle/server/service/TransactionService.java
+++ bigtangle-server/src/main/java/net/bigtangle/server/service/TransactionService.java
@@ -209,7 +209,8 @@ public class TransactionService {
             boolean override) throws Exception {
 
         Block block = createOrderMatchingBlock(prevHash, prevTrunk, prevBranch, override);
-        blockgraph.add(block, false);
+        if (block != null)
+            blockgraph.add(block, false);
         return block;
     }
 
@@ -307,7 +308,8 @@ public class TransactionService {
             boolean override) throws Exception {
 
         Block block = createMiningRewardBlock(prevRewardHash, prevTrunk, prevBranch, override);
-        blockgraph.add(block, false);
+        if (block != null)
+            blockgraph.add(block, false);
         return block;
     }
 
