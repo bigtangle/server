42dae0a2d905aedf3f7b4783d4a269f3f573924e
cui <cui@bigtangle.net>
2019-05-30 23:40:22 +0200

    hazecast cache

--- bigtangle-server/src/main/java/net/bigtangle/server/config/CachingConfig.java
+++ bigtangle-server/src/main/java/net/bigtangle/server/config/CachingConfig.java
@@ -6,35 +6,26 @@ import org.springframework.context.annotation.Configuration;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizeConfig;
 
 @Configuration
- 
 
 public class CachingConfig {
-//
-//    @Bean
-//    public CacheManager cacheManager() {
-//        return new ConcurrentMapCacheManager("blocks");
-//    }
-    
+    //
+    // @Bean
+    // public CacheManager cacheManager() {
+    // return new ConcurrentMapCacheManager("blocks");
+    // }
 
-    
     @Bean
     public Config hazelCastConfig() {
- 
+
         Config config = new Config();
-        config.setInstanceName("hazelcast-cache");
- 
-        MapConfig allUsersCache = new MapConfig();
-        allUsersCache.setTimeToLiveSeconds(20);
-        allUsersCache.setEvictionPolicy(EvictionPolicy.LFU);
-        config.getMapConfigs().put("alluserscache",allUsersCache);
- 
-        MapConfig usercache = new MapConfig();
-        usercache.setTimeToLiveSeconds(20);
-        usercache.setEvictionPolicy(EvictionPolicy.LFU);
-        config.getMapConfigs().put("usercache",usercache);
- 
+        config.setInstanceName("hazelcast-instance")
+                .addMapConfig(new MapConfig().setName("configuration")
+                        .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                        .setEvictionPolicy(EvictionPolicy.LRU).setTimeToLiveSeconds(3600));
         return config;
+
     }
 }
