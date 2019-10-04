849fe14b77b9c5e3543d7e3c5ee4ec312bc8831e
jiangtao <jiangtao19890902@sina.com>
2019-10-04 15:30:08 +0800

    test

--- bigtangle-tools/src/test/java/net/bigtangle/tools/WalletPayMoneyTest.java
+++ bigtangle-tools/src/test/java/net/bigtangle/tools/WalletPayMoneyTest.java
@@ -3,10 +3,13 @@ package net.bigtangle.tools;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.junit.Test;
 
-import net.bigtangle.core.Address;
 import net.bigtangle.core.Coin;
 import net.bigtangle.core.ECKey;
 import net.bigtangle.core.Utils;
@@ -17,15 +20,19 @@ public class WalletPayMoneyTest extends AbstractIntegrationTest {
     @Test
     public void pay() throws Exception {
         importKeys(walletAppKit2.wallet());
-        importKeys(walletAppKit1.wallet());
+        //importKeys(walletAppKit1.wallet());
+        this.wallet1();
+        this.wallet2();
 
-        walletAppKit1.wallet().setServerURL(HTTPS_BIGTANGLE_LOCAL);
-        walletAppKit2.wallet().setServerURL(HTTPS_BIGTANGLE_LOCAL);
+        walletAppKit1.wallet().setServerURL(HTTPS_BIGTANGLE_DE);
+        walletAppKit2.wallet().setServerURL(HTTPS_BIGTANGLE_DE);
+
+        // checkBalance(Coin.valueOf(100, testPub), wallet1Keys.get(0));
 
         List<WalletAppKit> walletAppKits = new ArrayList<WalletAppKit>();
         for (int i = 10; i < 20; i++) {
             WalletAppKit walletAppKit = this.createAndInitWallet("bigtangle" + i);
-            walletAppKit.wallet().setServerURL(HTTPS_BIGTANGLE_LOCAL);
+            walletAppKit.wallet().setServerURL(HTTPS_BIGTANGLE_DE);
             walletAppKits.add(walletAppKit);
         }
 
@@ -34,8 +41,31 @@ public class WalletPayMoneyTest extends AbstractIntegrationTest {
             bookData.put(walletBook.getWalletID(), walletBook);
         }
 
-        while (true) {
+        final int count = bookData.size();
+        ExecutorService pool = Executors.newFixedThreadPool(count);
+        for (Entry<Integer, WalletBook> entry : bookData.entrySet()) {
+            pool.execute(new PayMoneyRunnable(entry.getValue()));
+        }
+        pool.shutdown();
+    }
 
+    public static class PayMoneyRunnable implements Runnable {
+
+        public PayMoneyRunnable(WalletBook walletBook) {
+            this.walletBook = walletBook;
+        }
+
+        private WalletBook walletBook;
+        
+        @Override
+        public void run() {
+            while (true) {
+                log.info("--------------------- walletID : " + walletBook.getWalletID() + " ---------------------");
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                }
+            }
         }
     }
 
@@ -49,11 +79,21 @@ public class WalletPayMoneyTest extends AbstractIntegrationTest {
                 ECKey ecKey = new ECKey();
                 walletAppKit.wallet().importKey(ecKey);
 
+                String tokenid = Utils.HEX.encode(fromkey.getPubKey());
+                System.out.println(testPub);
+                System.out.println(tokenid);
+
+                // walletAppKit1.wallet().pay(null, ecKey.toAddress(networkParameters), Coin.valueOf(2l, tokenid), "");
                 HashMap<String, Long> giveMoneyResult = new HashMap<String, Long>();
-                giveMoneyResult.put(ecKey.toAddress(networkParameters).toString(), 1l);
+                giveMoneyResult.put(ecKey.toAddress(networkParameters).toBase58(), 10l);
+                
+
+                fromkey =  ECKey.fromPrivateAndPrecalculatedPublic(Utils.HEX.decode(testPriv), Utils.HEX.decode(testPub));
                 walletAppKit1.wallet().payMoneyToECKeyList(null, giveMoneyResult, fromkey);
                 
-                WalletKeyBook walletKeyBook = new WalletKeyBook(ecKey, this.sumUTXOBalance(Utils.HEX.encode(fromkey.getPubKey()), ecKey));
+//                walletAppKit.wallet().
+
+                WalletKeyBook walletKeyBook = new WalletKeyBook(ecKey, this.sumUTXOBalance(tokenid, ecKey));
                 walletBook.addWalletKeyBook(walletKeyBook);
             }
         }
