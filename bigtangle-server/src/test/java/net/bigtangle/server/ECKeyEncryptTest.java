1fd3ed93c1cd5d3b6b6b343ebbb56802a02d1774
jiangtao <jiangtao19890902@sina.com>
2019-12-06 22:32:15 +0800

    encrypt

--- bigtangle-server/src/test/java/net/bigtangle/server/ECKeyEncryptTest.java
+++ bigtangle-server/src/test/java/net/bigtangle/server/ECKeyEncryptTest.java
@@ -1,41 +1,42 @@
 package net.bigtangle.server;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import java.math.BigInteger;
+
 import org.ethereum.crypto.ECIESCoder;
 import org.junit.Test;
-import org.spongycastle.math.ec.ECPoint;
 import org.spongycastle.util.encoders.Hex;
 
 import net.bigtangle.core.ECKey;
 
-
 public class ECKeyEncryptTest {
-    
-    @Test  // encrypt decrypt round trip
-    public void test2(){
 
-        //BigInteger privKey = new BigInteger("5e173f6ac3c669587538e7727cf19b782a4f2fda07c1eaa662c593e5e85e3051", 16);
+    private byte[] payload = Hex.decode("1122334455");
 
-        byte[] payload = Hex.decode("1122334455");
-
-        //ECKey ecKey =  ECKey.fromPrivate(privKey);
-        
-        ECKey ecKey = new ECKey();
-        
-        ECPoint pubKeyPoint = ecKey.getPubKeyPoint();
-
-        byte[] cipher = new byte[0];
+    @Test
+    public void importECKeyDecrypt() {
+        BigInteger privKey = new BigInteger("5e173f6ac3c669587538e7727cf19b782a4f2fda07c1eaa662c593e5e85e3051", 16);
+        ECKey ecKey = ECKey.fromPrivate(privKey);
         try {
-            cipher = ECIESCoder.encrypt(pubKeyPoint, payload);
-        } catch (Throwable e) {e.printStackTrace();}
-
-        System.out.println(Hex.toHexString(cipher));
-
-        byte[] decrypted_payload = new byte[0];
-        try {
-            decrypted_payload = ECIESCoder.decrypt(ecKey.getPrivKey(), cipher);
-        } catch (Throwable e) {e.printStackTrace();}
-
-        System.out.println(Hex.toHexString(decrypted_payload));
+            byte[] cipher = ECIESCoder.encrypt(ecKey.getPubKeyPoint(), payload);
+            byte[] decrypted_payload = ECIESCoder.decrypt(ecKey.getPrivKey(), cipher);
+            assertArrayEquals(decrypted_payload, payload);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
-   
+
+    @Test
+    public void newECKeyDecrypt() {
+        ECKey ecKey = new ECKey();
+        try {
+            byte[] cipher = ECIESCoder.encrypt(ecKey.getPubKeyPoint(), payload);
+            byte[] decrypted_payload = ECIESCoder.decrypt(ecKey.getPrivKey(), cipher);
+            assertArrayEquals(decrypted_payload, payload);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
 }
