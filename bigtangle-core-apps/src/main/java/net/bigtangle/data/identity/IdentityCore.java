7d2655988457e1e60af222c70ed9eb9b5862724d
cui <cui@bigtangle.net>
2020-01-19 11:52:48 +0100

    add new project core-apps for identity

---
+++ bigtangle-core-apps/src/main/java/net/bigtangle/data/identity/IdentityCore.java
@@ -0,0 +1,98 @@
+package net.bigtangle.data.identity;
+
+public class IdentityCore {
+
+    private String surname;
+    private String forenames;
+    private String nationality;
+    private String dateofbirth;
+    private String sex;
+    private String placeofbirth;
+    private String dateofissue;
+    private String dateofexpiry;
+    private String authority;
+
+    private String nameatbirth;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getForenames() {
+        return forenames;
+    }
+
+    public void setForenames(String forenames) {
+        this.forenames = forenames;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPlaceofbirth() {
+        return placeofbirth;
+    }
+
+    public void setPlaceofbirth(String placeofbirth) {
+        this.placeofbirth = placeofbirth;
+    }
+
+    public String getDateofissue() {
+        return dateofissue;
+    }
+
+    public void setDateofissue(String dateofissue) {
+        this.dateofissue = dateofissue;
+    }
+
+    public String getDateofexpiry() {
+        return dateofexpiry;
+    }
+
+    public void setDateofexpiry(String dateofexpiry) {
+        this.dateofexpiry = dateofexpiry;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public String getNameatbirth() {
+        return nameatbirth;
+    }
+
+    public void setNameatbirth(String nameatbirth) {
+        this.nameatbirth = nameatbirth;
+    }
+    
+    
+}
