ab32cf922ee64a5933c86ea92ae135c1e89a8e06
cui <cui@bigtangle.net>
2020-01-20 17:16:47 +0100

    random number block

---
+++ bigtangle-server/src/test/java/net/bigtangle/server/RandomNumberBlock.java
@@ -0,0 +1,48 @@
+package net.bigtangle.server;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import net.bigtangle.core.Block;
+import net.bigtangle.core.Utils;
+
+public class RandomNumberBlock extends AbstractIntegrationTest {
+
+    @Test
+    public void testRandomNumber() throws Exception {
+        List<Block> a1 = new ArrayList<Block>(); 
+        List<Integer> rnumbers = new ArrayList<Integer>(); 
+        for(int i=0; i<6; i++) {
+            createRandomNumber(a1, rnumbers);
+        }
+        log.debug(rnumbers+" \n");
+    }
+    public void createRandomNumber(  List<Block> a1, List<Integer> rnumbers) throws Exception { 
+  
+        Block r1 = networkParameters.getGenesisBlock();
+        for (int i = 0; i < 3; i++) {
+            r1 = createReward(r1, a1);
+        }  
+        // Deterministic randomization
+        byte[] randomness = Utils.xor(r1.getPrevBlockHash().getBytes(), r1.getPrevBranchBlockHash().getBytes());
+        SecureRandom se = new SecureRandom(randomness);
+        int l1 = se.nextInt() % 33;
+    
+        rnumbers.add(Math.abs(l1));
+    }
+    public Block createReward(Block rewardBlock1, List<Block> blocksAddedAll) throws Exception {
+        for (int j = 1; j < 3; j++) {
+            sendEmpty(j);
+        }
+
+        // Generate mining reward block
+        Block next = rewardService.createReward(rewardBlock1.getHash());
+        blocksAddedAll.add(next);
+
+        return next;
+    }
+
+}
--- bigtangle-tools/src/main/java/net/bigtangle/tools/test/PartsToOne.java
+++ bigtangle-tools/src/main/java/net/bigtangle/tools/test/PartsToOne.java
@@ -4,16 +4,24 @@
  *******************************************************************************/
 package net.bigtangle.tools.test;
 
+import java.io.File;
 import java.math.BigInteger;
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongycastle.crypto.params.KeyParameter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import net.bigtangle.core.Address;
 import net.bigtangle.core.Block;
+import net.bigtangle.core.Coin;
+import net.bigtangle.core.ECKey;
 import net.bigtangle.core.NetworkParameters;
+import net.bigtangle.kits.WalletAppKit;
+import net.bigtangle.params.MainNetParams;
+import net.bigtangle.params.TestParams;
 
 public class PartsToOne extends HelpTest {
 
@@ -27,13 +35,38 @@ public class PartsToOne extends HelpTest {
         setUp();
         importKeys(walletAppKit2.wallet());
         importKeys(walletAppKit1.wallet());
-        
-       Block b= walletAppKit1.wallet().payPartsToOne(null, walletAppKit1.wallet().walletKeys().get(0).toAddress(networkParameters), NetworkParameters.BIGTANGLE_TOKENID,
-                "parts to one", new BigInteger("200000000"));
-       log.debug("  " +b.toString() );
-       Block b2= walletAppKit2.wallet().payPartsToOne(null, walletAppKit2.wallet().walletKeys().get(0).toAddress(networkParameters), NetworkParameters.BIGTANGLE_TOKENID,
-                "parts to one",  new BigInteger("200000000"));
-        log.debug("  " +b2.toString() );
+
+        Block b = walletAppKit1.wallet().payPartsToOne(null,
+                walletAppKit1.wallet().walletKeys().get(0).toAddress(networkParameters),
+                NetworkParameters.BIGTANGLE_TOKENID, "parts to one", new BigInteger("200000000"));
+        log.debug("  " + b.toString());
+        Block b2 = walletAppKit2.wallet().payPartsToOne(null,
+                walletAppKit2.wallet().walletKeys().get(0).toAddress(networkParameters),
+                NetworkParameters.BIGTANGLE_TOKENID, "parts to one", new BigInteger("200000000"));
+        log.debug("  " + b2.toString());
     }
-   
+
+    @Test
+    public void payParts() throws JsonProcessingException, Exception {
+
+        for (int i = 0; i < 5; i++) {
+            Block b = walletAdmin().wallet().pay(null,
+                    Address.fromBase58(MainNetParams.get(), "163LYA8FiLm4kVNLyvURavML6okkhaFPTf"),
+                    Coin.valueOf(8888000000l), "mining together");
+
+            log.debug("  " + b.toString());
+            Thread.sleep(50000);
+        }
+    }
+
+    protected WalletAppKit walletAdmin() throws Exception {
+        KeyParameter aesKey = null;
+
+        WalletAppKit w = new WalletAppKit(MainNetParams.get(), new File("/home/cui/Downloads"), "201811210100000002");
+        w.wallet().setServerURL("https://p.bigtangle.org:8088/");
+
+        return w;
+        // .wallet().walletKeys(aesKey).get(0) ;
+    }
+
 }
