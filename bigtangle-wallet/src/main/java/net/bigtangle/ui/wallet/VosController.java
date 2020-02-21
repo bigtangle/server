a48eab6a011cde88308b48d2287cc436363a2e50
cui <cui@bigtangle.net>
2020-02-21 11:11:39 +0100

    vos

--- bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/VosController.java
+++ bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/VosController.java
@@ -4,8 +4,6 @@
  *******************************************************************************/
 package net.bigtangle.ui.wallet;
 
-import static net.bigtangle.ui.wallet.utils.GuiUtils.checkGuiThread;
-
 import java.io.File;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -13,9 +11,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.spongycastle.crypto.params.KeyParameter;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,19 +19,15 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import net.bigtangle.core.Block;
-import net.bigtangle.core.Coin;
 import net.bigtangle.core.ECKey;
 import net.bigtangle.core.KeyValue;
 import net.bigtangle.core.MultiSignAddress;
-import net.bigtangle.core.Token;
 import net.bigtangle.core.TokenInfo;
 import net.bigtangle.core.TokenType;
 import net.bigtangle.core.Utils;
 import net.bigtangle.encrypt.ECIESCoder;
 import net.bigtangle.ui.wallet.utils.FileUtil;
 import net.bigtangle.ui.wallet.utils.GuiUtils;
-import net.bigtangle.ui.wallet.utils.IgnoreServiceException;
-import net.bigtangle.utils.MonetaryFormat;
 
 @SuppressWarnings("rawtypes")
 public class VosController {
@@ -78,14 +69,6 @@ public class VosController {
                 GuiUtils.informationalAlert("", Main.getText("signnumberNoEq"), "");
                 return;
             }
-
-            if (signnumberTF2id.getText() != null && !signnumberTF2id.getText().trim().isEmpty()
-                    && signnumberTF2id.getText().matches("[1-9]\\d*")
-                    && Long.parseLong(signnumberTF2id.getText().trim()) == 1) {
-
-                multiSinglePublish2id(Main.getAesKey(), issuedKeys);
-                return;
-            }
             if (signnumberTF2id.getText() != null && !signnumberTF2id.getText().trim().isEmpty()
                     && signnumberTF2id.getText().matches("[1-9]\\d*")
                     && Long.parseLong(signnumberTF2id.getText().trim()) > signAddrChoiceBox2id.getItems().size()) {
@@ -122,37 +105,13 @@ public class VosController {
             Main.walletAppKit.wallet().multiSign(currentToken.getToken().getTokenid(), outKey, Main.getAesKey());
 
             // tabPane.getSelectionModel().clearAndSelect(4);
-        } catch (IgnoreServiceException e) {
-        } catch (Exception e) {
+        }  
+        catch (Exception e) {
             GuiUtils.crashAlert(e);
         }
     }
 
-    private void multiSinglePublish2id(KeyParameter aesKey, List<ECKey> issuedKeys)
-            throws JsonProcessingException, Exception {
-        TokenInfo tokenInfo = new TokenInfo();
-        int decimals = 1;
-        BigInteger amount = BigInteger.ZERO;
-        Token tokens = Token.buildSimpleTokenInfo(false, null, tokenid2id.getValue().trim(),
-                tokenname2id.getText().trim(), "", 1, 0, amount, true, decimals, null);
-
-        tokenInfo.setToken(tokens);
-        ECKey mykey = null;
-        for (ECKey key : issuedKeys) {
-            if (key.getPublicKeyAsHex().equalsIgnoreCase(tokens.getTokenid())) {
-                mykey = key;
-            }
-        }
-
-        tokenInfo.getMultiSignAddresses().add(new MultiSignAddress(tokens.getTokenid(), "", mykey.getPublicKeyAsHex()));
-        Coin basecoin = MonetaryFormat.FIAT.noCode().parse("1", Utils.HEX.decode(tokenid2id.getValue()), decimals);
-
-        Main.walletAppKit.wallet().saveToken(tokenInfo, basecoin, mykey, aesKey);
-        GuiUtils.informationalAlert("", Main.getText("s_c_m"));
-        Main.instance.controller.initTableView();
-        checkGuiThread();
-        return;
-    }
+ 
     
     public void selectFile(ActionEvent event) {
 
