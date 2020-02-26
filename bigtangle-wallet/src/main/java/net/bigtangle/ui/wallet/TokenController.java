66bef35828e934d4f7b51c3154098ba3ec2c2a91
mi <mi@inasset.de>
2020-02-26 09:59:14 +0800

    fix bug

--- bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/TokenController.java
+++ bigtangle-wallet/src/main/java/net/bigtangle/ui/wallet/TokenController.java
@@ -4,10 +4,14 @@
  *******************************************************************************/
 package net.bigtangle.ui.wallet;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javafx.event.ActionEvent;
+import net.bigtangle.core.DataClassName;
+import net.bigtangle.ui.wallet.utils.GuiUtils;
 
 public class TokenController extends TokenPublishController {
 
@@ -15,5 +19,23 @@ public class TokenController extends TokenPublishController {
 
     public void closeUI(ActionEvent event) {
         overlayUI.done();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void add2positve(ActionEvent event) {
+        String CONTEXT_ROOT = Main.getContextRoot();
+        Map<String, Object> rowData = tokensTable.getSelectionModel().getSelectedItem();
+        if (rowData == null || rowData.isEmpty()) {
+            GuiUtils.informationalAlert(Main.getText("ex_c_m1"), Main.getText("ex_c_d1"));
+            return;
+        }
+
+        try {
+            Main.addToken(CONTEXT_ROOT, rowData.get("tokenname").toString() + ":" + rowData.get("asmarket"),
+                    rowData.get("tokenid").toString(), DataClassName.TOKEN.name());
+            GuiUtils.informationalAlert("", Main.getText("addwatchedSuccess"), "");
+        } catch (Exception e) {
+            GuiUtils.crashAlert(e);
+        }
     }
 }
