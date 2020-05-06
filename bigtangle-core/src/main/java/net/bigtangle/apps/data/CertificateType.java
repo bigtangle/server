2fb3c707822d21765cafede482d55f4cb7990d84
cui <cui@bigtangle.net>
2020-05-06 18:16:07 +0200

    certificate type

--- bigtangle-core/src/main/java/net/bigtangle/apps/data/CertificateType.java
+++ bigtangle-core/src/main/java/net/bigtangle/apps/data/CertificateType.java
@@ -1,7 +1,25 @@
 package net.bigtangle.apps.data;
 
 public enum CertificateType {
-    any, vehicleRegistration, bachlorDegree, masterDegree, doctorDegree, 
-    birthCertificate,weddingCertificate, realEstate, driveLicence,
+    //凭证
+    certificate, 
+    //车辆登记
+    vehicleRegistration, 
+    //学士文凭
+    bachlorDegree, 
+    //硕士学位
+    masterDegree, 
+    //博士学位
+    doctorDegree, 
+   //出生证
+    birthCertificate,
+    //结婚证
+    marriageCertificate, 
+    //房产证
+    realEstate, 
+    //驾照
+    driveLicence,
+    //专利
+    patent,
     
 }
