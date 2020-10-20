ccc2a910149bfd30e39da2fde714f430523896d6
cui <cui@bigtangle.net>
2020-10-20 15:26:50 +0200

    add unique token name for identity

--- bigtangle-core/src/main/java/net/bigtangle/apps/data/IdentityData.java
+++ bigtangle-core/src/main/java/net/bigtangle/apps/data/IdentityData.java
@@ -7,6 +7,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 import net.bigtangle.core.DataClass;
+import net.bigtangle.core.Sha256Hash;
 import net.bigtangle.core.Utils;
 
 public class IdentityData extends DataClass implements java.io.Serializable {
@@ -22,29 +23,29 @@ public class IdentityData extends DataClass implements java.io.Serializable {
     private byte[] photo;
     private byte[] machinereadable;
     // identity in English
-    IdentityCore identityCoreEn = new  IdentityCore();
+    IdentityCore identityCoreEn = new IdentityCore();
     // original language UTF-8
-    IdentityCore identityCore = new  IdentityCore();
+    IdentityCore identityCore = new IdentityCore();
 
-   
     public byte[] toByteArray() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
             DataOutputStream dos = new DataOutputStream(baos);
             dos.write(super.toByteArray());
             Utils.writeNBytesString(dos, code);
-            Utils.writeNBytesString(dos, identificationnumber); 
-            Utils.writeNBytes(dos,signatureofholder);
-            Utils.writeNBytes(dos,photo);
-            Utils.writeNBytes(dos,machinereadable); 
+            Utils.writeNBytesString(dos, identificationnumber);
+            Utils.writeNBytes(dos, signatureofholder);
+            Utils.writeNBytes(dos, photo);
+            Utils.writeNBytes(dos, machinereadable);
             dos.write(identityCoreEn.toByteArray());
-            dos.write( identityCore.toByteArray());
+            dos.write(identityCore.toByteArray());
             dos.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return baos.toByteArray();
     }
+
     public IdentityData parse(byte[] buf) throws IOException {
         ByteArrayInputStream bain = new ByteArrayInputStream(buf);
         DataInputStream dis = new DataInputStream(bain);
@@ -58,20 +59,42 @@ public class IdentityData extends DataClass implements java.io.Serializable {
 
     public IdentityData parseDIS(DataInputStream dis) throws IOException {
         super.parseDIS(dis);
-       
-        code= Utils.readNBytesString(dis );
-        identificationnumber= Utils.readNBytesString(dis); 
-        signatureofholder=  Utils.readNBytes(dis );
-        photo= Utils.readNBytes(dis );
-        machinereadable=  Utils.readNBytes(dis ); 
+
+        code = Utils.readNBytesString(dis);
+        identificationnumber = Utils.readNBytesString(dis);
+        signatureofholder = Utils.readNBytes(dis);
+        photo = Utils.readNBytes(dis);
+        machinereadable = Utils.readNBytes(dis);
         identityCoreEn = new IdentityCore().parseDIS(dis);
         identityCore = new IdentityCore().parseDIS(dis);
         dis.close();
- 
+
         return this;
     }
 
-    
+    /*
+     * This is unique token name for a identity using Hash
+     */
+    public String uniqueNameIdentity() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        try {
+            Utils.writeNBytesString(dos, identificationnumber);
+            Utils.writeNBytesString(dos, identityCore.getSurname());
+            Utils.writeNBytesString(dos, identityCore.getForenames());
+            Utils.writeNBytesString(dos, identityCore.getDateofbirth());
+            return Sha256Hash.wrap(baos.toByteArray()).toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                dos.close();
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
     public String getCode() {
         return code;
     }
@@ -126,5 +149,5 @@ public class IdentityData extends DataClass implements java.io.Serializable {
 
     public void setIdentityCore(IdentityCore identityCore) {
         this.identityCore = identityCore;
-    } 
+    }
 }
