8d93fb8d272497c56f8d7782f6768d92532a26cc
cui <cui@bigtangle.net>
2022-01-18 00:25:53 +0100

    burned

--- bigtangle-server/src/main/java/net/bigtangle/server/config/BurnedAddress.java
+++ bigtangle-server/src/main/java/net/bigtangle/server/config/BurnedAddress.java
@@ -11,31 +11,31 @@ public class BurnedAddress {
     public static List<BurnedAddress> init() {
         List<BurnedAddress> re = new ArrayList<BurnedAddress>();
         BurnedAddress a = new BurnedAddress("1718BePa7qEhNb24gyw3xZDvjsrCa6it5H", 549937L);
-        re.add(a);
-
-        a = new BurnedAddress("1MSHr1E2n8XogsPs2bP5qkzB6AUTpzXaQS", 552561L);
-        re.add(a);
-
-        a = new BurnedAddress("19tGEFb1ghRDsp2jCfB7rZYJiM23QHPxZi", 552561L);
-        re.add(a);
-
-        a = new BurnedAddress("19tJCQa3ioY172rXhRKwGCCBXnoFFVDYTJ", 552561L);
-        re.add(a);
-
-        a = new BurnedAddress("141JK2qkCRJxZbFZ9qbocRvMMpEEGa4tGj", 552561L);
-        re.add(a);
-        
-        a = new BurnedAddress("1MymcnqHbDErpj8fvt2eDnn1bmFqsPh65S", 552561L);
         re.add(a); 
-        
-        a = new BurnedAddress("1Hd9oqy9WBJUy3ApAvL5Zkjr8uiB4cKSaL", 552561L);
-        re.add(a); 
-        
-        a = new BurnedAddress("1ACA4Yt4YYwCxiVDR2SpcYN7XuvAeCGU7s", 552561L);
-        re.add(a); 
-        
+        long chain2 = 552648;
+        a = new BurnedAddress("1MSHr1E2n8XogsPs2bP5qkzB6AUTpzXaQS", chain2);
+        re.add(a);
+
+        a = new BurnedAddress("19tGEFb1ghRDsp2jCfB7rZYJiM23QHPxZi", chain2);
+        re.add(a);
+
+        a = new BurnedAddress("19tJCQa3ioY172rXhRKwGCCBXnoFFVDYTJ", chain2);
+        re.add(a);
+
+        a = new BurnedAddress("141JK2qkCRJxZbFZ9qbocRvMMpEEGa4tGj", chain2);
+        re.add(a);
+
+        a = new BurnedAddress("1MymcnqHbDErpj8fvt2eDnn1bmFqsPh65S", chain2);
+        re.add(a);
+
+        a = new BurnedAddress("1Hd9oqy9WBJUy3ApAvL5Zkjr8uiB4cKSaL", chain2);
+        re.add(a);
+
+        a = new BurnedAddress("1ACA4Yt4YYwCxiVDR2SpcYN7XuvAeCGU7s", chain2);
+        re.add(a);
+
         // for test
-        a = new BurnedAddress("1PqtKWvCUuPJf9YDK2WQAXqb3aeoav42Yh", 552561L);
+        a = new BurnedAddress("1PqtKWvCUuPJf9YDK2WQAXqb3aeoav42Yh", chain2);
         re.add(a);
 
         return re;
