d2f4e65317942650b42de43f59601f429ec1cc34
cui <cui@bigtangle.net>
2022-01-18 00:26:24 +0100

    burned

--- bigtangle-server/src/main/java/net/bigtangle/server/service/ValidatorService.java
+++ bigtangle-server/src/main/java/net/bigtangle/server/service/ValidatorService.java
@@ -1441,8 +1441,8 @@ public class ValidatorService {
     private Boolean checkBurnedFromAddress(final Transaction tx, Long chain) {
         String fromAddress = fromAddress(tx);
         for (BurnedAddress burned : BurnedAddress.init()) {
-            logger.debug(" checkBurnedFromAddress " + fromAddress + "  " + burned.getLockaddress() + " " + chain + " "
-                    + burned.getChain());
+ //           logger.debug(" checkBurnedFromAddress " + fromAddress + "  " + burned.getLockaddress() + " " + chain + " "
+ //                   + burned.getChain());
             if (burned.getLockaddress().equals(fromAddress) &&  chain >= burned.getChain() ) {
                 return true;
             }
