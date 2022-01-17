57fd206ab325d688124eec5698af8470bdd89f6e
cui <cui@bigtangle.net>
2022-01-17 23:18:10 +0100

    burned

--- bigtangle-server/src/main/java/net/bigtangle/server/config/BurnedAddress.java
+++ bigtangle-server/src/main/java/net/bigtangle/server/config/BurnedAddress.java
@@ -13,23 +13,29 @@ public class BurnedAddress {
         BurnedAddress a = new BurnedAddress("1718BePa7qEhNb24gyw3xZDvjsrCa6it5H", 549937L);
         re.add(a);
 
-        a = new BurnedAddress("1MSHr1E2n8XogsPs2bP5qkzB6AUTpzXaQS", 549937L);
+        a = new BurnedAddress("1MSHr1E2n8XogsPs2bP5qkzB6AUTpzXaQS", 552561L);
         re.add(a);
 
-        a = new BurnedAddress("19tGEFb1ghRDsp2jCfB7rZYJiM23QHPxZi", 549937L);
+        a = new BurnedAddress("19tGEFb1ghRDsp2jCfB7rZYJiM23QHPxZi", 552561L);
         re.add(a);
 
-        a = new BurnedAddress("19tJCQa3ioY172rXhRKwGCCBXnoFFVDYTJ", 549937L);
+        a = new BurnedAddress("19tJCQa3ioY172rXhRKwGCCBXnoFFVDYTJ", 552561L);
         re.add(a);
 
-        a = new BurnedAddress("141JK2qkCRJxZbFZ9qbocRvMMpEEGa4tGj", 549937L);
+        a = new BurnedAddress("141JK2qkCRJxZbFZ9qbocRvMMpEEGa4tGj", 552561L);
         re.add(a);
         
-        a = new BurnedAddress("1MymcnqHbDErpj8fvt2eDnn1bmFqsPh65S", 549937L);
+        a = new BurnedAddress("1MymcnqHbDErpj8fvt2eDnn1bmFqsPh65S", 552561L);
+        re.add(a); 
+        
+        a = new BurnedAddress("1Hd9oqy9WBJUy3ApAvL5Zkjr8uiB4cKSaL", 552561L);
+        re.add(a); 
+        
+        a = new BurnedAddress("1ACA4Yt4YYwCxiVDR2SpcYN7XuvAeCGU7s", 552561L);
         re.add(a); 
         
         // for test
-        a = new BurnedAddress("1PqtKWvCUuPJf9YDK2WQAXqb3aeoav42Yh", 1L);
+        a = new BurnedAddress("1PqtKWvCUuPJf9YDK2WQAXqb3aeoav42Yh", 552561L);
         re.add(a);
 
         return re;
