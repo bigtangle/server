9c86d01b56b0241931fa8899d9744209add465bb
cui <cui@bigtangle.net>
2023-01-29 11:35:25 +0100

    kyper

---
+++ bigtangle-core/src/main/java/net/bigtangle/core/KyperKey.java
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ *  Copyright   2018  Inasset GmbH. 
+ *  
+ *******************************************************************************/
+/*
+ * Copyright 2011 Google Inc.
+ * Copyright 2014 Andreas Schildbach
+ * Copyright 2014-2016 the libsecp256k1 contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.bigtangle.core;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.security.SignatureException;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import javax.annotation.Nullable;
+
+import org.bitcoin.NativeSecp256k1;
+import org.bitcoin.NativeSecp256k1Util;
+import org.bitcoin.Secp256k1Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongycastle.asn1.ASN1InputStream;
+import org.spongycastle.asn1.ASN1Integer;
+import org.spongycastle.asn1.DERBitString;
+import org.spongycastle.asn1.DEROctetString;
+import org.spongycastle.asn1.DERSequenceGenerator;
+import org.spongycastle.asn1.DERTaggedObject;
+import org.spongycastle.asn1.DLSequence;
+import org.spongycastle.asn1.x9.X9ECParameters;
+import org.spongycastle.asn1.x9.X9IntegerConverter;
+import org.spongycastle.crypto.AsymmetricCipherKeyPair;
+import org.spongycastle.crypto.digests.SHA256Digest;
+import org.spongycastle.crypto.ec.CustomNamedCurves;
+import org.spongycastle.crypto.generators.ECKeyPairGenerator;
+import org.spongycastle.crypto.params.ECDomainParameters;
+import org.spongycastle.crypto.params.ECKeyGenerationParameters;
+import org.spongycastle.crypto.params.ECPrivateKeyParameters;
+import org.spongycastle.crypto.params.ECPublicKeyParameters;
+import org.spongycastle.crypto.params.KeyParameter;
+import org.spongycastle.crypto.signers.ECDSASigner;
+import org.spongycastle.crypto.signers.HMacDSAKCalculator;
+import org.spongycastle.math.ec.ECAlgorithms;
+import org.spongycastle.math.ec.ECPoint;
+import org.spongycastle.math.ec.FixedPointCombMultiplier;
+import org.spongycastle.math.ec.FixedPointUtil;
+import org.spongycastle.math.ec.custom.sec.SecP256K1Curve;
+import org.spongycastle.util.encoders.Base64;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.UnsignedBytes;
+
+import net.bigtangle.crypto.EncryptableItem;
+import net.bigtangle.crypto.EncryptedData;
+import net.bigtangle.crypto.KeyCrypter;
+import net.bigtangle.crypto.KeyCrypterException;
+import net.bigtangle.crypto.LazyECPoint;
+import net.bigtangle.crypto.LinuxSecureRandom;
+import net.bigtangle.crypto.TransactionSignature;
+import net.bigtangle.utils.DumpedPrivateKey;
+import net.bigtangle.wallet.Protos;
+import net.bigtangle.wallet.Protos.Wallet.EncryptionType;
+import net.bigtangle.wallet.Wallet;
+
+// TODO: Move this class to tracking compression state itself.
+// The Bouncy Castle developers are deprecating their own tracking of the compression state.
+
+/**
+ * <p>Represents an elliptic curve public and (optionally) private key, usable for digital signatures but not encryption.
+ * Creating a new ECKey with the empty constructor will generate a new random keypair. Other static methods can be used
+ * when you already have the public or private parts. If you create a key with only the public part, you can check
+ * signatures but not create them.</p>
+ *
+ * <p>ECKey also provides access to Bitcoin Core compatible text message signing, as accessible via the UI or JSON-RPC.
+ * This is slightly different to signing raw bytes - if you want to sign your own data and it won't be exposed as
+ * text to people, you don't want to use this. If in doubt, ask on the mailing list.</p>
+ *
+ * <p>The ECDSA algorithm supports <i>key recovery</i> in which a signature plus a couple of discriminator bits can
+ * be reversed to find the public key used to calculate it. This can be convenient when you have a message and a
+ * signature and want to find out who signed it, rather than requiring the user to provide the expected identity.</p>
+ *
+ * <p>This class supports a variety of serialization forms. The methods that accept/return byte arrays serialize
+ * private keys as raw byte arrays and public keys using the SEC standard byte encoding for public keys. Signatures
+ * are encoded using ASN.1/DER inside the Bitcoin protocol.</p>
+ *
+ * <p>A key can be <i>compressed</i> or <i>uncompressed</i>. This refers to whether the public key is represented
+ * when encoded into bytes as an (x, y) coordinate on the elliptic curve, or whether it's represented as just an X
+ * co-ordinate and an extra byte that carries a sign bit. With the latter form the Y coordinate can be calculated
+ * dynamically, however, <b>because the binary serialization is different the address of a key changes if its
+ * compression status is changed</b>. If you deviate from the defaults it's important to understand this: money sent
+ * to a compressed version of the key will have a different address to the same key in uncompressed form. Whether
+ * a public key is compressed or not is recorded in the SEC binary serialisation format, and preserved in a flag in
+ * this class so round-tripping preserves state. Unless you're working with old software or doing unusual things, you
+ * can usually ignore the compressed/uncompressed distinction.</p>
+ */
+public class KyperKey implements EncryptableItem {
+
+    @Override
+    public boolean isEncrypted() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public byte[] getSecretBytes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public EncryptedData getEncryptedData() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public EncryptionType getEncryptionType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getCreationTimeSeconds() {
+        // TODO Auto-generated method stub
+        return 0;
+    } 
+}
