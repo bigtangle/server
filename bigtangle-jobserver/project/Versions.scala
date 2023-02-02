c3ca33a60e1c1755de10e234ccfeb676ac8bad3a
cui <cui@bigtangle.net>
2023-02-02 23:07:57 +0100

    version and remove job server

--- bigtangle-jobserver/project/Versions.scala
+++
@@ -1,6 +0,0 @@
-object Versions {
-  lazy val netty = "4.0.33.Final"
-  lazy val jobServer = "0.8.0"
-  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.3.0")
-  lazy val typesafeConfig = "1.2.1"
-}
