6505c317449ba23ca2cd3e7271626b633b86bcce
tianj <mi@bigtangle.net>
2023-02-16 18:15:53 +0800

    assert

---
+++ bigtangle-core/src/test/java/net/bigtangle/script/Test.java
@@ -0,0 +1,41 @@
+package net.bigtangle.script;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import org.bouncycastle.util.encoders.Hex;
+
+import net.thiim.dilithium.impl.PackingUtils;
+import net.thiim.dilithium.interfaces.DilithiumParameterSpec;
+import net.thiim.dilithium.interfaces.DilithiumPrivateKey;
+import net.thiim.dilithium.interfaces.DilithiumPrivateKeySpec;
+import net.thiim.dilithium.interfaces.DilithiumPublicKey;
+import net.thiim.dilithium.interfaces.DilithiumPublicKeySpec;
+import net.thiim.dilithium.provider.DilithiumProvider;
+
+public class Test {
+    @org.junit.Test
+    public void test() throws Exception {
+        DilithiumProvider pv = new DilithiumProvider();
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("Dilithium", pv);
+        kpg.initialize(DilithiumParameterSpec.LEVEL2);
+        KeyPair kp = kpg.generateKeyPair();
+
+        DilithiumPrivateKey sk = (DilithiumPrivateKey) kp.getPrivate();
+        DilithiumPublicKey pk = (DilithiumPublicKey) kp.getPublic();
+        System.out.println("pk = " + Hex.toHexString(pk.getEncoded()).toUpperCase());
+        System.out.println("sk = " + Hex.toHexString(sk.getEncoded()).toUpperCase());
+
+        DilithiumPublicKeySpec pubspec = new DilithiumPublicKeySpec(DilithiumParameterSpec.LEVEL2, pk.getEncoded());
+        PublicKey publicKey = PackingUtils.unpackPublicKey(pubspec.getParameterSpec(), pubspec.getBytes());
+        System.out.println("pk1 = " + Hex.toHexString(publicKey.getEncoded()).toUpperCase());
+
+        DilithiumPrivateKeySpec prvspec = new DilithiumPrivateKeySpec(DilithiumParameterSpec.LEVEL2, sk.getEncoded());
+        PrivateKey privateKey = PackingUtils.unpackPrivateKey(prvspec.getParameterSpec(), prvspec.getBytes());
+        System.out.println("sk2 = " + Hex.toHexString(privateKey.getEncoded()).toUpperCase());
+
+    }
+}
