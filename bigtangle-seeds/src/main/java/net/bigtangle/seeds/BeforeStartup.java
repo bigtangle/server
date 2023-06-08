c147fdd0d7d85a0e455525fe46ffad756062516c
tianj <mi@bigtangle.net>
2023-06-08 13:41:56 +0800

    local file

--- bigtangle-seeds/src/main/java/net/bigtangle/seeds/BeforeStartup.java
+++ bigtangle-seeds/src/main/java/net/bigtangle/seeds/BeforeStartup.java
@@ -4,6 +4,14 @@
  *******************************************************************************/
 package net.bigtangle.seeds;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,18 +21,58 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class BeforeStartup {
 
-    private static final Logger logger = LoggerFactory.getLogger(BeforeStartup.class);
+	private static final Logger logger = LoggerFactory.getLogger(BeforeStartup.class);
 
-    @PostConstruct
-    public void run() throws Exception {
+	@PostConstruct
+	public void run() throws Exception {
 
-    	 /*
-    	 * At Start read   server info list
-    	 */
-        
-       
+		/*
+		 * At Start read server info list
+		 */
+		String path = "d:/java/serverinfo.json";
+		File file = new File(path);
+		if (file.exists()) {
+			String jsonString = file2String(file);
+			if (jsonString != null && !"".equals(jsonString.trim())) {
+				JSONArray jsonArray = new JSONArray(jsonString);
+				for (int i = 0; i < jsonArray.length(); i++) {
+					JSONObject jsonObject = jsonArray.getJSONObject(i);
+					ServerInfo serverInfo = new ServerInfo();
+					serverInfo.setUrl(jsonObject.getString("url"));
+					serverInfo.setServertype(jsonObject.getString("servertype"));
+					serverInfo.setChain(jsonObject.getString("chain"));
+					serverInfo.setStatus(jsonObject.getString("status"));
+					if (DispatcherController.serverinfoList == null) {
+						DispatcherController.serverinfoList = new ArrayList<ServerInfo>();
+						DispatcherController.serverinfoList.add(serverInfo);
+					}
 
-    }
+				}
 
-   
+			}
+		}
+
+	}
+
+	public static String file2String(final File file) throws IOException {
+		if (file.exists()) {
+			byte[] data = new byte[(int) file.length()];
+			boolean result;
+			FileInputStream inputStream = null;
+			try {
+				inputStream = new FileInputStream(file);
+				int len = inputStream.read(data);
+				result = len == data.length;
+			} finally {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			}
+			if (result) {
+				return new String(data);
+			}
+		}
+		return null;
+	}
+
 }
--- bigtangle-seeds/src/main/java/net/bigtangle/seeds/SyncBlockService.java
+++ bigtangle-seeds/src/main/java/net/bigtangle/seeds/SyncBlockService.java
@@ -4,20 +4,22 @@
  *******************************************************************************/
 package net.bigtangle.seeds;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import net.bigtangle.core.NetworkParameters;
 import net.bigtangle.core.TXReward;
 import net.bigtangle.core.exception.BlockStoreException;
 import net.bigtangle.core.response.GetTXRewardResponse;
@@ -48,22 +50,76 @@ public class SyncBlockService {
 	public void startSingleProcess() throws BlockStoreException, JsonProcessingException, IOException {
 
 		log.debug(" Start syncServerInfo  : ");
-	//	syncServerInfo();
+		// syncServerInfo();
 
 		log.debug(" end syncServerInfo: ");
 
 	}
 
-	/* write to local file and read it at start only  
+	/*
+	 * write to local file and read it at start only
 	 */
 	public void localFileServerInfoWrite() throws JsonProcessingException, IOException {
-	 
+
+		if (DispatcherController.serverinfoList != null) {
+			String path = "d:/java/serverinfo.json";
+			File file = new File(path);
+			if (file.exists()) {
+				file.delete();
+			}
+			String jsonString = Json.jsonmapper().writeValueAsString(DispatcherController.serverinfoList);
+			writeString2File(jsonString, path);
+		}
+
 	}
 
-	
- 
+	public static File writeString2File(String Data, String filePath)
 
-		
+	{
+
+		BufferedReader bufferedReader = null;
+
+		BufferedWriter bufferedWriter = null;
+
+		File distFile = null;
+
+		try {
+
+			distFile = new File(filePath);
+
+			if (!distFile.getParentFile().exists())
+				distFile.getParentFile().mkdirs();
+
+			bufferedReader = new BufferedReader(new StringReader(Data));
+
+			bufferedWriter = new BufferedWriter(new FileWriter(distFile));
+
+			char buf[] = new char[1024]; // 字符缓冲区
+
+			int len;
+
+			while ((len = bufferedReader.read(buf)) != -1)
+
+			{
+
+				bufferedWriter.write(buf, 0, len);
+
+			}
+
+			bufferedWriter.flush();
+
+			bufferedReader.close();
+
+			bufferedWriter.close();
+
+		} catch (Exception e) {
+
+		}
+
+		return distFile;
+
+	}
+
 	public void syncServerInfo() throws JsonProcessingException, IOException {
 
 		List<String> badserver = new ArrayList<String>();
@@ -76,7 +132,6 @@ public class SyncBlockService {
 			data = OkHttp3Util.post(s + ReqCmd.serverinfolist.name(),
 					Json.jsonmapper().writeValueAsString(requestParam).getBytes());
 			ServerinfoResponse response = Json.jsonmapper().readValue(data, ServerinfoResponse.class);
- 
 
 		}
 		// check each server data
@@ -85,12 +140,12 @@ public class SyncBlockService {
 
 	private void checkChain(ServerinfoResponse response) throws JsonProcessingException, IOException {
 		// update the list DispatcherController.serverinfo;
-		long chainLength=0;
+		long chainLength = 0;
 		if (response.getServerInfoList() != null) {
 			for (ServerInfo serverInfo : response.getServerInfoList()) {
 				TXReward txReward = getMaxConfirmedReward(serverInfo.getUrl());
-				if (txReward.getChainLength()>=chainLength) {
-					chainLength=txReward.getChainLength();
+				if (txReward.getChainLength() >= chainLength) {
+					chainLength = txReward.getChainLength();
 				}
 
 			}
