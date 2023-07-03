d550140c92d828d545c79de803519edc71ffa9eb
tianj <mi@bigtangle.net>
2023-07-03 17:31:44 +0800

    send conf file to w01

--- bigtangle-web/src/test/java/net/bigtangle/web/test/WebTest.java
+++ bigtangle-web/src/test/java/net/bigtangle/web/test/WebTest.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Base64;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,8 @@ import net.bigtangle.core.TokenType;
 import net.bigtangle.core.Utils;
 import net.bigtangle.params.TestParams;
 import net.bigtangle.wallet.Wallet;
+import net.bigtangle.web.SyncBlockService;
+import net.bigtangle.web.Zip;
 
 public class WebTest extends AbstractIntegrationTest {
 
@@ -54,6 +57,49 @@ public class WebTest extends AbstractIntegrationTest {
 		ECKey signkey = ECKey.fromPrivate(Utils.HEX.decode(testPriv));
 
 		wallet.multiSign(contractKey.getPublicKeyAsHex(), signkey, null);
+		byte[] zipFile = Base64.decodeBase64("zipcontent");
+		SyncBlockService.byte2File(zipFile, "/var/www/", "contractlottery" + ".zip");
+		new Zip().unZip("/var/www/" + "contractlottery" + ".zip");
+		StringBuffer stringBuffer = new StringBuffer();
+		
+		stringBuffer.append("<VirtualHost *:80>\n");
+		stringBuffer.append("ServerName contractlottery.bigtangle.org\n");
+		stringBuffer.append("DocumentRoot /var/www/contractlottery\n");
+		stringBuffer.append("ErrorLog ${APACHE_LOG_DIR}/error.log\n");
+		stringBuffer.append("CustomLog ${APACHE_LOG_DIR}/access.log combined\n");
+		stringBuffer.append("RewriteEngine on\n");
+		stringBuffer.append("RewriteCond %{SERVER_NAME} =contractlottery.bigtangle.org\n");
+		stringBuffer.append("RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]\n");
+		stringBuffer.append("</VirtualHost>\n");
+		
+		stringBuffer.append("<VirtualHost *:443>\n");
+		stringBuffer.append("ServerName  contractlottery.bigtangle.org\n");
+		stringBuffer.append("DocumentRoot /var/www/contractlottery\n");
+		stringBuffer.append("ErrorLog ${APACHE_LOG_DIR}/error.log\n");
+		stringBuffer.append("CustomLog ${APACHE_LOG_DIR}/access.log combined\n");
+		stringBuffer.append("SSLProxyEngine on\n");
+		stringBuffer.append("SSLProxyVerify none\n");
+		stringBuffer.append("SSLProxyCheckPeerCN off\n");
+		stringBuffer.append("SSLProxyCheckPeerName off\n");
+		stringBuffer.append("SSLProxyCheckPeerExpire off\n");
+		stringBuffer.append("    SSLEngine on\n");
+		stringBuffer.append("    SSLOptions +StrictRequire\n");
+		stringBuffer.append("SSLProxyVerify none\n");
+		stringBuffer.append("SSLProxyCheckPeerCN off\n");
+		stringBuffer.append(" <Directory />\n");
+		stringBuffer.append("SSLRenegBufferSize 2098200000\n");
+		stringBuffer.append("SSLRequireSSL\n");
+		stringBuffer.append("</Directory>\n");
+		stringBuffer.append(" SSLCipherSuite HIGH:MEDIUM:!aNULL:+SHA1:+MD5:+HIGH:+MEDIUM\n");
+		stringBuffer.append(" SSLSessionCacheTimeout 600\n");
+		stringBuffer.append("SSLProxyEngine on\n");
+		stringBuffer.append("Include /etc/letsencrypt/options-ssl-apache.conf\n");
+		stringBuffer.append("SSLCertificateFile /etc/letsencrypt/live/www.bigtangle.org/fullchain.pem\n");
+		stringBuffer.append("SSLCertificateKeyFile /etc/letsencrypt/live/www.bigtangle.org/privkey.pem\n");
+		stringBuffer.append("</VirtualHost>");
+		SyncBlockService.byte2File(stringBuffer.toString().getBytes(), "/var/www/", "contractlottery.bigtangle.org" + ".conf");
+		
+		
 
 	}
 
